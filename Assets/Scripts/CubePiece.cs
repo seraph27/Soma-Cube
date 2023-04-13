1e86fd478f52f766f8b86344b43267b4b92255a4
will <will1711.dev@gmail.com>
2023-04-12 21:44:33 -0700

    rotation

--- Assets/Scripts/CubePiece.cs
+++ Assets/Scripts/CubePiece.cs
@@ -6,10 +6,29 @@ using ControlFreak2;
 
 public class CubePiece : MonoBehaviour
 {
+    public GameObject[] Cubes;
+
     // Start is called before the first frame update
-    void Start()
+    public void AdjustPivotToMidPoint()
     {
-        
+        Vector3 midPoint = Vector3.zero;
+        Vector3 iniPos = transform.position;
+
+        //Gets the middle point of a piece, used for rotation
+        foreach (GameObject cube in Cubes)
+        {
+            midPoint += cube.transform.position;
+        }
+        midPoint /= Cubes.Length;
+
+        transform.position = midPoint;
+
+
+        Vector3 cubeShiftPos = iniPos - midPoint;
+        foreach (GameObject cube in Cubes)
+        {
+            cube.transform.position += cubeShiftPos;
+        }
     }
 
     // Update is called once per frame
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,7 +1,9 @@
 using ControlFreak2;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -11,9 +13,14 @@ public class PlayerManager : MonoBehaviour
     [Header("Select Object Handler")]
     public Transform SelectReferencePosition;
     public float swipeThreshold;
+    public float swipeSpeed = 2f;
+
+    public float rotationIncrement = 90f;
 
     bool Rotating;
     GameObject currentPiece;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -39,20 +46,39 @@ public class PlayerManager : MonoBehaviour
         RotationUpdate(x,y);
     }
 
+    //Select a piece and bring it in front of the screen
     void SelectPiece(GameObject PieceSelected)
     {
         PieceSelected.transform.position = SelectReferencePosition.position;
         currentPiece = PieceSelected;
+
+        CubePiece piece = currentPiece.GetComponent<CubePiece>();
+
+        piece.AdjustPivotToMidPoint(); //change the pivot point of the piece to be the middle of the cubes
+
+        piece.transform.position = SelectReferencePosition.transform.position;
+        piece.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
     }
 
     void RotationUpdate(float rotationX, float rotationY)
     {
-        if(Rotating)
+        if (Rotating)
         {
             // currentPiece.transform.rotation *= Camera.transform.tra Quaternion.Euler(rotationX, rotationY, 0);
-            currentPiece.transform.RotateAround(currentPiece.transform.position, Camera.transform.up, rotationX);
-            currentPiece.transform.RotateAround(currentPiece.transform.position, Camera.transform.right, rotationY);
+            currentPiece.transform.RotateAround(currentPiece.transform.position, -transform.up, rotationX * swipeSpeed);
+            currentPiece.transform.RotateAround(currentPiece.transform.position, transform.right, rotationY * swipeSpeed);
+        }
+        else
+        {
+            if (currentPiece != null)
+            {
+                float x = (Mathf.Round(currentPiece.transform.eulerAngles.x / rotationIncrement) * rotationIncrement);
+                float y = (Mathf.Round(currentPiece.transform.eulerAngles.y / rotationIncrement) * rotationIncrement);
+                float z = (Mathf.Round(currentPiece.transform.eulerAngles.z / rotationIncrement) * rotationIncrement);
 
+
+                currentPiece.transform.eulerAngles = new Vector3(x,y,z);
+            }
         }
     }
 
